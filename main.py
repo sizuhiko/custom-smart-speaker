9208aae34d130de03995e1d4a141c6dbeffca923
sizuhiko <k-kishida@esm.co.jp>
2018-09-19 16:13:09 +0900

    add ir control

--- main.py
+++ main.py
@@ -21,14 +21,62 @@ import time
 import os
 import sys
 import uuid
+import smbus
 
 aiy.i18n.set_language_code('ja-JP')
 myuuid = str(uuid.uuid4())
 model = os.path.join(os.path.dirname(__file__), 'hotword.pmdl')
 
+# for RPI version 1, use "bus = smbus.SMBus(0)"
+bus = smbus.SMBus(1)
+
+# This must match in the Arduino Sketch
+#SLAVE_ADDRESS = 0x04
+SLAVE_ADDRESS = 0x52
+data_numH = 0x31
+data_numL = 0x32
+data_numHL = [0x00,0x31,0x32]
+data_num = 10
+memo_no = 0
+block = []
+
+#command
+R1_memo_no_write = 0x15  #bus-write(ADR,cmd,1)
+R2_data_num_read = 0x25 #bus-read(ADR,cmd,3)
+R3_data_read           = 0x35 #bus-read(ADR,cmd,n)
+W1_memo_no_write  = 0x19 #bus-write(ADR,cmd,1)
+W2_data_num_write = 0x29 #bus-write(ADR,cmd,3)
+W3_data_write           = 0x39 #bus-read(ADR,cmd,n)
+W4_flash_write           = 0x49 #bus-read(ADR,cmd,n)
+T1_trans_start             = 0x59 #bus-write(ADR,cmd,1)
+
+def trans_command(block2):
+  str_tmp = ""
+  int_tmp = []
+  for i in range(int(len(block2)//2)):
+    str_tmp = block2[i*2] + block2[i*2+1]
+    int_tmp.append( int(str_tmp, 16))
+  # cmd W2_data_num_write 0x29 bus-write(ADR,cmd,3)
+  data_num = int(len(int_tmp)//4)  #for test
+  data_numHL = [0x31,0x32] #for test
+  data_numHL[0] = int(data_num//256)
+  data_numHL[1] = int(data_num%256)
+  bus.write_i2c_block_data(SLAVE_ADDRESS, W2_data_num_write ,  data_numHL)   #= 
+  data_numHL = [0x31,0x32,0x33,0x34] #for test 
+  for i in range(data_num):
+    data_numHL[0] = int_tmp[i*4+0]
+    data_numHL[1] = int_tmp[i*4+1]
+    data_numHL[2] = int_tmp[i*4+2]
+    data_numHL[3] = int_tmp[i*4+3]
+    bus.write_i2c_block_data(SLAVE_ADDRESS, W3_data_write , data_numHL)   #= 
+  # cmd T1_trans_start             0x59 bus-write(ADR,cmd,1)
+  memo_no = [0x00 ] #for dummy
+  bus.write_i2c_block_data(SLAVE_ADDRESS, T1_trans_start,memo_no )   #= 
+
 # SKILL's function
 def tv_on():
   say("TVの電源を入れます")
+  trans_command('5B0018002E001800180018002E001800170018002E00190017001800170018002E00180018001800170018001700180017004F03')
 
 # SKILL map
 skills = {
